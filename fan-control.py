9a8cafe91f6f3e24d051f92de3e058ca32335e05
James Blanton <jamespblanton@gmail.com>
2023-01-17 12:25:56 -0500

    touched up topic list; working

--- fan-control.py
+++ fan-control.py
@@ -1,29 +1,66 @@
 import argparse
+import atexit
+
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
-# what to put in main() and what to spread out
-# wrapper for changing fan settings?
-# also, HIGH is off and LOW and is on
-def main(pins, topic, host):
-    GPIO.setmode(GPIO.BCM)
-    for n in pins:
-        GPIO.setup(n, GPIO.OUT)
+
+def cleanup(client):
+    GPIO.cleanup()
+    print("Cleaning up")
+    client.loop_stop()
+
 
 def on_message(client, userdata, message):
-    #tentX/fanY/status
+    # tentX/fanY/status
     # look into how to subscribe since we'll need a few
+    payload = message.payload
+    place, device, metric = message.topic.split("/")
+    pin = userdata[device]
+    if payload == b"True":
+        GPIO.output(pin, GPIO.LOW)
+    elif payload == b"False":
+        GPIO.output(pin, GPIO.HIGH)
 
-if __name__=='__main__':
+
+def mqtt_connect(host, topics, userdata):
+    client = mqtt.Client(userdata=userdata)
+    client.connect(host, 1883)
+    for topic in topics:
+        client.subscribe(topic)
+    return client
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpio', type=int, nargs='+', required=True, help='List of integers representing GPIO pins')
-    parser.add_argument('--topic', type=str, required=True, help='Topic for the script to publish to')
-    parser.add_argument('--host', type=str, required=True, help='Hostname or IP of the MQTT broker')
+    parser.add_argument(
+        "--host", type=str, required=True, help="Hostname or IP of the MQTT broker"
+    )
+    parser.add_argument(
+        "--gpio",
+        type=int,
+        nargs="+",
+        required=True,
+        help="List of integers representing GPIO pins",
+    )
+    parser.add_argument(
+        "--topics",
+        type=str,
+        nargs="+",
+        required=True,
+        help="Topics for the script to publish to",
+    )
     args = parser.parse_args()
-
+    GPIO.setmode(GPIO.BCM)
+    for pin in args.gpio:
+        GPIO.setup(pin, GPIO.OUT)
     n_fans = len(args.gpio)
-    
-    client = mqtt.Client()
-    client.connect(args.host, 1883)
-    client.subscribe(args.topic)
-    client.loop_start()
+
+    userdata = {}
+    for i, pin in enumerate(args.gpio):
+        userdata[f"fan{i+1}"] = pin
+
+    client = mqtt_connect(args.host, args.topics, userdata)
+    client.on_message = on_message
+    atexit.register(cleanup, client)
+    client.loop_forever()
